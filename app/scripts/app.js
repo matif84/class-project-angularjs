6434b8506558c6abb43bcad6d645e170e58de76d
Muhammad Atif <muhammadatif@bridge.home>
2014-08-09 10:39:54 -0400

    Adding empty file structure

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appointment1App
+ * @description
+ * # appointment1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appointment1App', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appointment1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appointment1App
+ */
+angular.module('appointment1App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
